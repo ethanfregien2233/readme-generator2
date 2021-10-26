af1730abc87ab8dfb71362b48510ab110e115b5b
ethan fregien <ethanfregien@gmail.com>
2021-10-26 15:06:10 -0500

    added badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,8 @@ function generateReadme(answers) {
   return `
 <h1 align="center">${answers.projectTitle}</h1>
 
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+
 ## Description
 ${answers.description}
 ## Table of Contents
