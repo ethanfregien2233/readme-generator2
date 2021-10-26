b3e6fa97d1f036bb5ed282836190d2718dcbe4a9
ethan fregien <ethanfregien@gmail.com>
2021-10-26 13:14:22 -0500

    started index

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
-// TODO: Include packages needed for this application
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateMarkdown")
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project's title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process (if applicable) ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project used for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose the license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "List project contributors"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Are tests included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What should I do if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
